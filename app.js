94b1806bd51fab3920210576f1a42ba883688f70
Zetsuboii <hmzkrb@gmail.com>
2021-03-20 08:20:31 +0300

    add middleware to get request time, update nft object

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const hash = crypto.createHash('sha256');
 const encrypt = (i) => {
@@ -14,33 +18,26 @@ const encrypt = (i) => {
   return hash.digest('hex');
 };
 
-const dataPathFmt = (name) =>
-  `${__dirname}/data/${name}.json`;
+const dataPathFmt = (name) => `${__dirname}/data/${name}.json`;
 
-const nfts = JSON.parse(
-  fs.readFileSync(dataPathFmt('nft'))
-);
+const nfts = JSON.parse(fs.readFileSync(dataPathFmt('nft')));
 
 const updateNftsFile = () => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(
-      dataPathFmt('nft'),
-      JSON.stringify(nfts),
-      (err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
+    fs.writeFile(dataPathFmt('nft'), JSON.stringify(nfts), (err) => {
+      if (err) {
+        reject(err);
+        return;
       }
-    );
+      resolve();
+    });
   });
 };
 
-const addBufferIndex = (d) =>
-  Buffer.concat([d, Buffer.from(nfts.length + '')]);
+const addBufferIndex = (d) => Buffer.concat([d, Buffer.from(nfts.length + '')]);
 
 const getAllNfts = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: nfts.length,
@@ -80,14 +77,15 @@ const addNft = (req, res) => {
       }
       filePath = addBufferIndex(filePath);
       const hashId = encrypt(filePath);
-      const entry = Object.assign({ id: hashId }, fields);
+      const entry = Object.assign(
+        { id: hashId, createdTime: req.requestTime },
+        fields
+      );
       //? Not sure how to handle files in here correctly, since I'll use DB I won't bother
       nfts.push(entry);
       updateNftsFile()
         .then(() => {
-          console.log(
-            '✔ Contents of nft.json have been updated'
-          );
+          console.log('✔ Contents of nft.json have been updated');
           res.status(200).json({
             status: 'success',
             data: entry,
@@ -115,9 +113,7 @@ const changeOwner = (req, res) => {
       }
       const nft = nfts.find((n) => n.id === req.params.id);
       if (nft === undefined) {
-        console.log(
-          '❌ No NFT found with id ' + req.params.id
-        );
+        console.log('❌ No NFT found with id ' + req.params.id);
         res.status(404).json({
           status: 'fail',
           msg: 'invalid ID',
@@ -127,21 +123,16 @@ const changeOwner = (req, res) => {
       if (fields.address !== nft.owner) {
         res.status(403).json({
           status: 'fail',
-          msg:
-            "can't change the ownership of the NFT you don't own",
+          msg: "can't change the ownership of the NFT you don't own",
         });
         return;
       }
 
       nft.owner = fields.owner;
-      console.log(
-        nfts.find((n) => n.id === req.params.id).owner
-      );
+      console.log(nfts.find((n) => n.id === req.params.id).owner);
       updateNftsFile()
         .then(() => {
-          console.log(
-            '✔ Contents of nft.json have been updated'
-          );
+          console.log('✔ Contents of nft.json have been updated');
           res.status(200).json({
             status: 'success',
             data: {
@@ -163,16 +154,9 @@ const changeOwner = (req, res) => {
   }
 };
 
-app.get('/api/v1/nft', getAllNfts);
-
-app.get('/api/v1/nft/:id', getNftOfId);
-
-app.post('/api/v1/nft', addNft);
-
-app.patch('/api/v1/nft/:id', changeOwner);
+app.route('/api/v1/nft').get(getAllNfts).post(addNft);
+app.route('/api/v1/nft/:id').get(getNftOfId).patch(changeOwner);
 
 //? So far I don't feel like I should have a delete method
 
-app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`)
-);
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
