4ace93f9caf0090ad21628b86c75e70c21132d0a
Zetsuboii <hmzkrb@gmail.com>
2021-03-21 17:21:07 +0300

    add mongoose connection and environment vars

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const PORT = 3000;
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection successful');
+  });
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(` 🌐 Example app listening on port ${PORT}!`)
 );
