ebd3c58de93f0fa66965bba142dbe66656dacc6e
Zetsuboii <hmzkrb@gmail.com>
2021-03-25 07:45:10 +0300

    add contract model

---
+++ models/nft/contractModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const contractMissing = (field) => `Contract is missing ${field} field`;
+
+const contractSchema = new mongoose.Schema({
+  hashedId: {
+    type: String,
+    required: [true, contractMissing('hashedId')],
+    unique: true,
+  },
+  createdTime: {
+    type: Date,
+    required: [true, contractMissing('createdTime')],
+  },
+  name: {
+    type: String,
+    required: [true, contractMissing('name')],
+  },
+  owner: {
+    type: String,
+    required: [true, nftMissing('owner')],
+  },
+});
+
+const Contract = mongoose.model('Contract', contractSchema);
+
+module.exports = Contract;
