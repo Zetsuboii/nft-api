6b7f0f2efe082c10ab2ef9154e24a02e0dec8598
Zetsuboii <hmzkrb@gmail.com>
2021-03-25 08:14:46 +0300

    getContract  method

--- controllers/contractController.js
+++ controllers/contractController.js
@@ -7,15 +7,18 @@ const contracts = JSON.parse(fs.readFileSync(utils.dataPathFmt('contracts')));
 exports.createContract = async (req, res) => {
   //* Will use "application/json"
   const fields = req.body;
+
   const contractLength = (await Contract.find()).length;
   const fieldsWithId = JSON.stringify(fields) + contractLength;
   const hashedId = utils.encrypt(fieldsWithId);
+
   const entry = {
     hashedId: hashedId,
     createdTime: new Date(),
     name: fields.name,
     owner: fields.owner,
   };
+
   Contract.create(entry)
     .then(() => {
       res.status(200).json({
@@ -31,17 +34,13 @@ exports.createContract = async (req, res) => {
     });
 };
 
-exports.getContract = (req, res) => {
+exports.getContract = async (req, res) => {
   //* 64 characters long id isn't necessarily expected, a matching one is enough
   //* The app should decide how long of a query it will send, 8 seems enough
 
-  //? That's a one way to do it but I want the first characters to match
-  /* const matches = contracts.filter((contract) =>
-      contract.id.includes(req.params.id, 0)
-    ); */
-  const matches = contracts.filter(
-    (contract) => contract.id.slice(0, req.params.id.length) === req.params.id
-  );
+  const matches = await Contract.find({
+    hashedId: { $regex: `${req.params.id}.*` },
+  });
   if (matches.length === 0) {
     res.status(404).json({
       status: 'fail',
